5b7594cc8a02b77fb21e7001d9a2856f182f73df
Carlton Schuyler <cschuyler@rallydev.com>
2012-10-04 14:28:09 -0400

    First Capacity Utilization by Rank app - changing name of this repo later

--- App.js
+++ App.js
@@ -1,120 +1,156 @@
+//this custom tree item shows the name and the object id of a story.
+ Ext.define('MyTreeItem', {
+     extend: 'Rally.ui.tree.TreeItem',
+     alias: 'widget.mytreeitem',
+
+     // override the function to create a new template for the pill
+     getPillTpl: function(){
+         return Ext.create('Ext.XTemplate',
+         '<div class="pill">',
+             '{FormattedID} -',
+             ' Plan Est: {PlanEstimate}',
+             ' Schedule State: {Schedule State}',
+             ' Task Est: {TaskEstimateTotal}',
+             ' Task Actual: {TaskActualTotal}',
+             ' Task Remaining: {TaskRemainingTotal}',
+             ' {Name}',
+             ' {Rank}',
+         '</div>'
+         );
+     }
+ });
+
 Ext.define('CustomApp', {
     extend: 'Rally.app.App',
     componentCls: 'app',
-    layout: {
-        type: 'vbox',
-        align: 'stretch'
-    },
-    items:[
-        {
+    
+    launch: function() {
+        this.add({
             xtype: 'panel',
-            layout: 'anchor',
-            border: true,
-            fieldDefaults: {
-                labelWidth: 40
+            layout: 'hbox',
+            width: 'stretch',
+            defaults: {
+                flex: 1
             },
-            defaultType: 'textfield',
-            bodyPadding: 5,
             items: [
                 {
-                    fieldLabel: 'Query',
-                    itemId: 'queryField',
-                    anchor:'100%',
-                    width: 700,
-                    height: 100,
-                    xtype: 'textarea',
-                    value: '{\n'+
-                            '    "ObjectID": {$gt:0},\n'+
-                            '    "__At": "current"\n'+
-                            '}'
+                    xtype: 'rallydatefield',
+                    itemId: 'reportStartDate',
+                    fieldLabel: 'Start Date',
+                    value: new Date(2012, 7, 1)
                 },
                 {
-                    fieldLabel: 'Fields',
-                    itemId: 'fieldsField',
-                    anchor: '100%',
-                    width: 700,
-                    value: "ObjectID, _ValidFrom, _UnformattedID, Name"
-                },
-                {
-                    fieldLabel: 'Sort',
-                    itemId: 'sortField',
-                    anchor: '100%',
-                    width: 700,
-                    value: "{'ObjectID' : -1, '_ValidFrom': 1}"
-                },
-                {
-                    fieldLabel: 'Page Size',
-                    itemId: 'pageSizeField',
-                    anchor: '100%',
-                    width: 700,
-                    value: '10'
-                }
-            ],
-            
-            buttons: [
-                {
-                    xtype: 'rallybutton',
-                    text: 'Search',
-                    itemId: 'searchButton'
+                    xtype: 'rallydatefield',
+                    itemId: 'reportEndDate',
+                    fieldLabel: 'End Date',
+                    value: new Date(2012, 7, 31)
                 }
             ]
-        },
-        {
+        });
+
+        this.add({
+            xtype: 'panel',
+            title: 'Capacity Utilization by Rank',
+            layout: 'hbox',
+            width: 'stretch',
+            items: [
+                {
+                xtype: 'panel',
+                flex: 1,
+                defaultType: 'textfield',
+                defaults: {
+                    padding: 10
+                },
+                items: [
+                    {
+                        xtype: 'rallytree',
+                        itemId: 'rallyTree',
+                        fieldLabel: 'Filter By Epic Story',
+                        listeners: {
+                            itemselected: this._userStorySelected,
+                            scope: this
+                        },
+                        topLevelStoreConfig: {
+                            sorters: [
+                                {
+                                    property: 'Rank'
+                                }
+                            ],
+                            pageSize: 30
+                        },
+                        treeItemConfigForRecordFn: function(record){
+                            //only use the new tree item for stories
+                            if(record.get('_type') === 'hierarchicalrequirement'){
+                                return {
+                                    xtype: 'mytreeitem',
+                                    selectable: true
+                                };
+                            } else {
+                                return {
+                                    selectable: true
+                                };
+                            }
+
+                        }
+                    }
+                    // {
+                    //     xtype: 'textarea',
+                    //     fieldLabel: 'Query',
+                    //     itemId: 'queryField',
+                    //     height: 100,
+                    //     value:  '{\n'+
+                    //             '    "State": "Completed",\n'+
+                    //             '    "Actuals": {$gte: 0},\n' +
+                    //             '    "_TypeHierarchy": -51009,\n' +
+                    //             '    "_ValidTo": "9999-01-01T00:00:00.000Z"\n' +
+                    //             '}'
+                    // },
+                ]
+           },
+           {
             xtype: 'panel',
             itemId: 'gridHolder',
-            layout: 'fit',
-            height: 400
-        }
-    ],
-    launch: function() {
-        var button = this.down('#searchButton');
-        button.on('click', this.searchClicked, this);
+            flex: 2
+            }
+        ]});
     },
-    
-    searchClicked: function(){
-        var queryField = this.down('#queryField');
-        var query = queryField.getValue();
-        var selectedFields = this.down('#fieldsField').getValue();
-        if(selectedFields){
-            if(selectedFields === 'true'){
-                selectedFields = true;
-            }
-            else{
-                selectedFields = selectedFields.split(', ');
-            }
-        }
-        
-        var sort = this.down('#sortField').getValue();
-        
-        var pageSize = this.down('#pageSizeField').getValue();
-        var parsedPageSize = parseInt(pageSize, 10);
-        // don't allow empty or 0 pagesize
-        pageSize = (parsedPageSize) ? parsedPageSize : 10;
 
-        var callback = Ext.bind(this.processSnapshots, this);
-        this.doSearch(query, selectedFields, sort, pageSize, callback);
-    },
-    
-    createSortMap: function(csvFields){
-        var fields = csvFields.split(', ');
-        var sortMap = {};
-        for(var field in fields){
-            if(fields.hasOwnProperty(field)){
-                sortMap[field] = 1;
-            }
-        }
+    _getCapacity: function() {
+        //TODO: should we exclude non-working days?
+        var numOfDays = Rally.util.DateTime.getDifference(this._getEndDate(), this._getStartDate(), 'day');
+        var people = 5;
+        var hoursPerPoint = 3;
         
-        return sortMap;
+       return numOfDays*people*hoursPerPoint;
     },
     
-    doSearch: function(query, fields, sort, pageSize, callback){
-        var transformStore = Ext.create('Rally.data.lookback.SnapshotStore', {
+    _userStorySelected: function(treeItem){
+        var query = {
+            State: "Completed",
+            Actuals: {$gte: 0},
+            _TypeHierarchy: -51009
+        };
+    
+        var startDate = this._getStartDate();
+        var endDate = this._getEndDate();
+        // query._ValidFrom = {$gte:startDate,$lt:endDate};
+        query.__At = endDate;
+        query._ItemHierarchy = treeItem.getRecord().get('ObjectID');
+
+        var capacity = this._getCapacity(startDate, endDate);
+        
+        this._doSearch(Ext.JSON.encode(query));
+    },
+    
+    _doSearch: function(query){     
+        Ext.create('Rally.data.lookback.SnapshotStore', {
             context: {
                 workspace: this.context.getWorkspace(),
                 project: this.context.getProject()
             },
-            fetch: fields,
+            fetch:  ["ObjectID", "_ValidFrom", "_UnformattedID", "Name", "Actuals"],
             rawFind: query,
+            pageSize: 200,
             autoLoad: true,
             listeners: {
                 scope: this,
@@ -122,10 +158,26 @@ Ext.define('CustomApp', {
             }
         });
     },
+
+    _getStartDate: function() {
+        return this.down('#reportStartDate').getValue(); 
+    },
+
+    _getEndDate: function() {
+        return this.down('#reportEndDate').getValue();
+    },
     
     processSnapshots: function(store, records){
+        var snapshots = [];
+        store.each(function(record) {
+            snapshots.push(record.data);
+        });
+            
+        var capacity = this._getCapacity();
+        var totalActuals = Rally.data.util.Transform.aggregate(snapshots, [{field: 'Actuals', f: '$sum'}]);
+        
         var snapshotGrid = Ext.create('Rally.ui.grid.Grid', {
-            title: 'Snapshots',
+            title: 'Time Spent on What',
             store: store,
             columnCfgs: [
                 {
@@ -134,7 +186,12 @@ Ext.define('CustomApp', {
                 },
                 {
                     text: 'Name', 
-                    dataIndex: 'Name'
+                    dataIndex: 'Name',
+                    flex: 1
+                },
+                {
+                    text: 'Actuals', 
+                    dataIndex: 'Actuals'
                 },
                 {
                     text: 'Project',
@@ -142,11 +199,13 @@ Ext.define('CustomApp', {
                 },
                 {
                     text: '_ValidFrom',
-                    dataIndex: '_ValidFrom' 
+                    dataIndex: '_ValidFrom',
+                    flex: 1
                 },
                 {
                     text: '_ValidTo',
-                    dataIndex: '_ValidTo' 
+                    dataIndex: '_ValidTo', 
+                    flex: 1
                 }
             ],
             height: 400
@@ -154,6 +213,8 @@ Ext.define('CustomApp', {
         
         var gridHolder = this.down('#gridHolder');
         gridHolder.removeAll(true);
+        gridHolder.add({xtype: 'component', html: 'Total Actuals: ' + totalActuals.Actuals_$sum + ' Total Capacity: ' + capacity});
+        gridHolder.add({xtype: 'rallypercentdone', percentDone: totalActuals.Actuals_$sum/capacity});
         gridHolder.add(snapshotGrid);
     }
 });
